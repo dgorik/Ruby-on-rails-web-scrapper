2dd9e63ae781e5bc8231f90941d45e01700ac426
Daniel Gorbachev <dg186533@gmail.com>
2025-08-20 07:06:55 -0400

    changed data types on tests

--- test/services/extraction/word_counter_test.rb
+++ test/services/extraction/word_counter_test.rb
@@ -5,14 +5,14 @@ class Extraction::WordCounterTest < ActiveSupport::TestCase
     html = "<html><body>This is a simple test.</body></html>"
     doc = Nokogiri::HTML(html)
     result = Extraction::WordCounter.call(doc)
-    assert_equal 5, result
+    assert_equal "5", result
   end
 
   test "returns zero when body is empty" do
     html = "<html><body>   </body></html>"
     doc = Nokogiri::HTML(html)
     result = Extraction::WordCounter.call(doc)
-    assert_equal 0, result
+    assert_equal "0", result
   end
 end
 
